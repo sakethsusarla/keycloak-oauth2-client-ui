fe8311e55e129af7a221a358238cc4d90ee9e6ec
Saketh Susarla <venkata.susarla@siemens.com>
2023-11-07 09:19:00 +0530

    intial commit

---
+++ backend/src/main/java/com/example/keycloakoauth2clientui/KeycloakOauth2ClientUiApplication.java
@@ -0,0 +1,13 @@
+package com.example.keycloakoauth2clientui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakOauth2ClientUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakOauth2ClientUiApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/keycloakoauth2clientui/KeycloakOauth2ClientUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keycloakoauth2clientui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakOauth2ClientUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
